577d1e3602441ca4b06f9a98103daca0f281d4fa
Carl-Erik Bergström <carl-erik.bergstrom@klarna.com>
2017-02-23 11:15:18 +0100

    Added tests

---
+++ test/tests.test.js
@@ -0,0 +1,59 @@
+import { expect } from 'chai'
+import deserialize from '../lib/express-java-monetary-amount-deserializer'
+
+function next() {}
+
+describe('Nested tests', function () {
+
+  it('should expand MonetaryAmount in nested object', function () {
+    let request = {
+      body: {
+        goodsList: [
+          { title: 'Dancing shoes', amount: 'GBP 100.00' },
+          { title: 'Return of not so dancing shoes', amount: 'GBP -100.00' }
+        ]
+      }
+    }
+    let response = {}
+
+    deserialize(request, response, next)
+
+    expect(request.body.goodsList[0].amount.amount).to.equal('100.00')
+    expect(request.body.goodsList[0].amount.currency).to.equal('GBP')
+
+    expect(request.body.goodsList[1].amount.amount).to.equal('-100.00')
+    expect(request.body.goodsList[1].amount.currency).to.equal('GBP')
+
+    // Test string representation
+    expect(String(request.body.goodsList[0].amount)).to.equal('GBP 100.00')
+    expect(String(request.body.goodsList[1].amount)).to.equal('GBP -100.00')
+  })
+
+  it('should expand MonetaryAmount in really nested object', function () {
+    let request = {
+      body: {
+        computer: {
+          components: [
+            { type: 'ram', brand: 'GoodBrand Inc.', price: 'GBP 200.00' },
+            { type: 'cpu', brand: 'GoodBrand Inc.', price: 'GBP 300.00' }
+          ],
+          sum: 'GBP 500.00'
+        }
+      }
+    }
+    let response = {}
+
+    deserialize(request, response, next)
+
+    expect(request.body.computer.components[0].price.amount).to.equal('200.00')
+    expect(request.body.computer.components[1].price.amount).to.equal('300.00')
+
+    // Should not corrupt rest of the object
+    expect(request.body.computer.components[0].type).to.equal('ram')
+    expect(request.body.computer.components[1].type).to.equal('cpu')
+
+    // Test string representation
+    expect(String(request.body.computer.components[0].price)).to.equal('GBP 200.00')
+    expect(String(request.body.computer.components[1].price)).to.equal('GBP 300.00')
+  })
+})
